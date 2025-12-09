1549196a8ff441d368ab6fca7780b1baea8b7eab
otaviohtp-cmd <otaviohtp@gmail.com>
2025-12-09 11:50:08 +0000

    EX02.py

---
+++ Exercícios/EX02.py
@@ -0,0 +1,5 @@
+Palavra = input("Digite a palavra que será repetida CINCO vezes " )
+print (f"{Palavra}{Palavra}{Palavra}{Palavra}{Palavra}")
+
+Palavra900 = input("Digite a palavra que será repetida 10 vezes pq 900 vai demorar muito " )
+print (f"{Palavra900*10} ")
