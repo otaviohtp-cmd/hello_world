732ba4726422e520817c9226a81edd5d130f5b1c
otaviohtp-cmd <otaviohtp@gmail.com>
2025-12-10 00:45:00 +0000

    EX04 Completo

---
+++ Exercícios/EX04.py
@@ -0,0 +1,9 @@
+a = int(input("Digite o primeiro numero: "))
+b = int(input("Digite o segundo numero: "))
+
+soma = a + b
+multiplicação = a * b
+subtração = a - b
+divisão = a // b
+print("RESULTADO:")
+print(f"{a}x{b} = {multiplicação}\n{a}+{b} = {soma}\n{a}-{b} = {subtração}\n{a}/{b} = {divisão}")
