239b7c33ee31f772f609bfce8f8ddbc161533738
otaviohtp-cmd <otaviohtp@gmail.com>
2025-12-16 01:03:42 +0000

    Alteração

---
+++ Exercícios/EX08_listas.py
@@ -0,0 +1,9 @@
+lista_3_valores = [15,20,571]
+a = lista_3_valores[0]
+b = lista_3_valores[1]
+c = lista_3_valores[2]
+print(f"A soma de todos os números da lista é: {a+b+c}")
+print(f"a multiplicação de todos os números é: {a*b*c}")
+print(f"A divisão de todos os números da lista é: {a/b/c}")
+print(f"O quadrado de: \n{a} = {a*a}\n{b} = {b*b}\n{c} = {c*c}")
+
